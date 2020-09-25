f77c41d03b1e45ef6359e1315ae55c2a82e85224
EvgenyDrozdov <dart1402@gmail.com>
2020-09-25 18:41:23 +0300

    first

---
+++ app/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tSOOPEXDataSet = new app.TSOOPEXDataSet();
+            this.order_dBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.order_dTableAdapter = new app.TSOOPEXDataSetTableAdapters.order_dTableAdapter();
+            this.tableAdapterManager = new app.TSOOPEXDataSetTableAdapters.TableAdapterManager();
+            this.order_dBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.order_dBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.order_dDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tSOOPEXDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.order_dBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.order_dBindingNavigator)).BeginInit();
+            this.order_dBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.order_dDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tSOOPEXDataSet
+            // 
+            this.tSOOPEXDataSet.DataSetName = "TSOOPEXDataSet";
+            this.tSOOPEXDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // order_dBindingSource
+            // 
+            this.order_dBindingSource.DataMember = "order_d";
+            this.order_dBindingSource.DataSource = this.tSOOPEXDataSet;
+            // 
+            // order_dTableAdapter
+            // 
+            this.order_dTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.order_dTableAdapter = this.order_dTableAdapter;
+            this.tableAdapterManager.UpdateOrder = app.TSOOPEXDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // order_dBindingNavigator
+            // 
+            this.order_dBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.order_dBindingNavigator.BindingSource = this.order_dBindingSource;
+            this.order_dBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.order_dBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.order_dBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.order_dBindingNavigatorSaveItem});
+            this.order_dBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.order_dBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.order_dBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.order_dBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.order_dBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.order_dBindingNavigator.Name = "order_dBindingNavigator";
+            this.order_dBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.order_dBindingNavigator.Size = new System.Drawing.Size(800, 25);
+            this.order_dBindingNavigator.TabIndex = 0;
+            this.order_dBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Переместить в начало";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Переместить назад";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Положение";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Текущее положение";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(43, 15);
+            this.bindingNavigatorCountItem.Text = "для {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Общее число элементов";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Переместить вперед";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Переместить в конец";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Добавить";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Удалить";
+            // 
+            // order_dBindingNavigatorSaveItem
+            // 
+            this.order_dBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.order_dBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("order_dBindingNavigatorSaveItem.Image")));
+            this.order_dBindingNavigatorSaveItem.Name = "order_dBindingNavigatorSaveItem";
+            this.order_dBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.order_dBindingNavigatorSaveItem.Text = "Сохранить данные";
+            this.order_dBindingNavigatorSaveItem.Click += new System.EventHandler(this.order_dBindingNavigatorSaveItem_Click);
+            // 
+            // order_dDataGridView
+            // 
+            this.order_dDataGridView.AutoGenerateColumns = false;
+            this.order_dDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.order_dDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.order_dDataGridView.DataSource = this.order_dBindingSource;
+            this.order_dDataGridView.Location = new System.Drawing.Point(80, 42);
+            this.order_dDataGridView.Name = "order_dDataGridView";
+            this.order_dDataGridView.Size = new System.Drawing.Size(518, 220);
+            this.order_dDataGridView.TabIndex = 1;
+            this.order_dDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.order_dDataGridView_CellContentClick);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "order";
+            this.dataGridViewTextBoxColumn1.HeaderText = "order";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "order_date";
+            this.dataGridViewTextBoxColumn2.HeaderText = "order_date";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "order_desc";
+            this.dataGridViewTextBoxColumn3.HeaderText = "order_desc";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.order_dDataGridView);
+            this.Controls.Add(this.order_dBindingNavigator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tSOOPEXDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.order_dBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.order_dBindingNavigator)).EndInit();
+            this.order_dBindingNavigator.ResumeLayout(false);
+            this.order_dBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.order_dDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TSOOPEXDataSet tSOOPEXDataSet;
+        private System.Windows.Forms.BindingSource order_dBindingSource;
+        private TSOOPEXDataSetTableAdapters.order_dTableAdapter order_dTableAdapter;
+        private TSOOPEXDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator order_dBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton order_dBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView order_dDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+    }
+}
---
+++ app/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void order_dBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.order_dBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.tSOOPEXDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void order_dDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ConnectionStringSettingsCollection sett = ConfigurationManager.ConnectionStrings;
+            string conStr = sett[0].ConnectionString;
+            SqlConnection Connection = new SqlConnection(conStr);
+
+            SqlCommand sql = new SqlCommand();
+
+            sql.Connection = Connection;
+            Connection.Open();
+            sql.CommandText = "select * from [TSOOPEX].dbo.order_d";
+            Connection.Close();
+        }
+    }
+}
